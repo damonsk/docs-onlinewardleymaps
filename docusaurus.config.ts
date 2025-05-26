d3a36827071dc00f8d8cf9a221ff30321392ddd4
damonsk <dskelly16@gmail.com>
2025-05-26 17:26:20 +0100

    update url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://help.onlinewardleymaps.com',
+  url: 'https://docs.onlinewardleymaps.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
